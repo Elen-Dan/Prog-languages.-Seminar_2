80ef15d10a031bbb1b27f3f4a0854ae656c48473
El_Dan <lilit-li@ya.ru>
2022-08-26 14:24:12 +0400

    Внесение изменений в Task_1 и добавление Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 2: Напишите программу, которая выводит третью цифру случайно сгенерированного числа 
+(от 1 до 65536) или сообщает, что третьей цифры нет.
+*/
+
+Console.Clear();
+int RndNumber = new Random().Next(1, 65536); //Генерируем случайное число
+
+Console.WriteLine (RndNumber); // Показываем число на экране
+
+if (RndNumber < 100) {
+    Console.WriteLine("Третьей цифры в числе нет");
+}
+if (RndNumber >= 100 & RndNumber < 1000) {
+    RndNumber = RndNumber % 10; //узнаем третью цифру числа остатком от деления
+    Console.WriteLine("Третья цифра числа: " + RndNumber);
+}
+if (RndNumber >= 1000 & RndNumber < 10000) {
+    RndNumber = RndNumber / 10;
+    RndNumber = RndNumber % 10; //узнаем третью цифру числа остатком от деления
+    Console.WriteLine("Третья цифра числа: " + RndNumber);
+}
+if (RndNumber >= 10000 & RndNumber <= 65536) {
+    RndNumber = RndNumber / 100;
+    RndNumber = RndNumber % 10; //узнаем третью цифру числа остатком от деления
+    Console.WriteLine("Третья цифра числа: " + RndNumber);
+}
